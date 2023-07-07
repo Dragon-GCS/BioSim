1803ecfe5f84a79dee87d5087454a91bcd7c4aab
Dragon <878173121@qq.com>
2023-07-07 13:32:55 +0800

    feat: change Coordinate.index to cache property

--- biosim/_coordinate.py
+++ biosim/_coordinate.py
@@ -1,15 +1,20 @@
+from functools import cache
 from ._config import config
 
 
 class Coordinate:
     """保存各生物的坐标信息， 左下角为坐标原点，x轴向右，y轴向上，index为坐标在一维数组中的索引"""
 
-    __slots__ = ("x", "y", "index")
+    __slots__ = ("x", "y")
 
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
-        self.index = x + y * config.world.width
+
+    @property
+    @cache
+    def index(self):
+        return self.x + self.y * config.world.width
 
     def distance(self, other: "Coordinate") -> int:
         return abs(self.x - other.x) + abs(self.y - other.y)
@@ -17,13 +22,15 @@ class Coordinate:
     def radius(self, other: "Coordinate") -> int:
         return max(abs(self.x - other.x), abs(self.y - other.y))
 
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
+
     def __add__(self, other: "Coordinate") -> "Coordinate":
         return Coordinate(self.x + other.x, self.y + other.y)
 
     def __iadd__(self, other: "Coordinate"):
         self.x += other.x
         self.y += other.y
-        self.index += other.index
 
     def __sub__(self, other: "Coordinate") -> "Coordinate":
         return Coordinate(self.x - other.x, self.y - other.y)
@@ -31,7 +38,6 @@ class Coordinate:
     def __isub__(self, other: "Coordinate"):
         self.x -= other.x
         self.y -= other.y
-        self.index -= other.index
 
     def __eq__(self, other: "Coordinate") -> bool:
         return self.index == other.index
