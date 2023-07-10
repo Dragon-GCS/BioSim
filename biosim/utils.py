5d2871a3392770c95c53af8ecf184fd122d0b8e2
Dragon <878173121@qq.com>
2023-07-10 14:50:29 +0800

    feat: yield with radiums in utils.spiral_scan()

--- biosim/utils.py
+++ biosim/utils.py
@@ -3,20 +3,23 @@ from typing import Iterator
 from ._coordinate import Coordinate
 
 
-def spiral_scan(max_radius: int) -> Iterator[Coordinate]:
+def spiral_scan(max_radius: int) -> Iterator[tuple[int, Coordinate]]:
     """获取从原点开始，以螺旋扫描的方式遍历坐标系的坐标作为位移， 每一圈的遍历顺序为：v > ^ <。
     每圈都可以看作是从左下角开始移动的正方形，每个方向移动的步长等于2 * radius。但每个正方形的第一步都是向下移动的
     Args:
         max_radius: 最大半径
+    Yield:
+        radiums (int): 当前圈的半径
+        direction (Coordinate): 当前移动方向
     """
     # 定义螺旋扫描的方向，按右、下、左、上的顺序移动
     # directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]  # -> ^ <- v
     directions = [Coordinate(1, 0), Coordinate(0, 1), Coordinate(-1, 0), Coordinate(0, -1)]
 
     for radius in range(1, max_radius + 1):
-        yield directions[-1]
+        yield radius, directions[-1]
         for _ in range(1, 2 * radius):
-            yield directions[0]
+            yield radius, directions[0]
         for i in range(1, 4):
             for _ in range(1, 2 * radius + 1):
-                yield directions[i]
+                yield radius, directions[i]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,22 +10,22 @@ class TestUtils(TestCase):
         coordinates = list(spiral_scan(4))
         # fmt: off
         targets = [
-            Coordinate(0, -1), Coordinate(1, 0),    # v >
-            *[Coordinate(0, 1) for _ in range(2)],     # ^
-            *[Coordinate(-1, 0) for _ in range(2)],   # <
-            *[Coordinate(0, -1) for _ in range(2)],    # v
-            Coordinate(0, -1), *[Coordinate(1, 0)] * 3,    # v > > >
-            *[Coordinate(0, 1)] * 4,     # ^ ^ ^ ^
-            *[Coordinate(-1, 0)] * 4,   # < < < <
-            *[Coordinate(0, -1)] * 4,     # v v v v
-            Coordinate(0, -1), *[Coordinate(1, 0)] * 5,    # v > > > >
-            *[Coordinate(0, 1)] * 6,     # ^ ^ ^ ^ ^
-            *[Coordinate(-1, 0)] * 6,   # < < < < <
-            *[Coordinate(0, -1)] * 6,     # v v v v v
-            Coordinate(0, -1), *[Coordinate(1, 0)] * 7,    # v > ...
-            *[Coordinate(0, 1)] * 8,   # ^ ^ ...
-            *[Coordinate(-1, 0)] * 8,  # < < ...
-            *[Coordinate(0, -1)] * 8,  # v v ...
+            (1, Coordinate(0, -1)), (1, Coordinate(1, 0)),    # v >
+            *[(1, Coordinate(0, 1)) for _ in range(2)],     # ^
+            *[(1, Coordinate(-1, 0)) for _ in range(2)],   # <
+            *[(1, Coordinate(0, -1)) for _ in range(2)],    # v
+            (2, Coordinate(0, -1)), *[(2, Coordinate(1, 0))] * 3,    # v > > >
+            *[(2, Coordinate(0, 1))] * 4,     # ^ ^ ^ ^
+            *[(2, Coordinate(-1, 0))] * 4,   # < < < <
+            *[(2, Coordinate(0, -1))] * 4,     # v v v v
+            (3, Coordinate(0, -1)), *[(3, Coordinate(1, 0))] * 5,    # v > > > >
+            *[(3, Coordinate(0, 1))] * 6,     # ^ ^ ^ ^ ^
+            *[(3, Coordinate(-1, 0))] * 6,   # < < < < <
+            *[(3, Coordinate(0, -1))] * 6,     # v v v v v
+            (4, Coordinate(0, -1)), *[(4, Coordinate(1, 0))] * 7,    # v > ...
+            *[(4, Coordinate(0, 1))] * 8,   # ^ ^ ...
+            *[(4, Coordinate(-1, 0))] * 8,  # < < ...
+            *[(4, Coordinate(0, -1))] * 8,  # v v ...
         ]
         # fmt: on
         # 4个边，每边长度为2 * radiums
